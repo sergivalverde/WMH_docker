b0539d5db198febe8834aec3b7d425a6cf5741f5
Sergi Valverde <sergi.valverde@udg.edu>
2017-07-28 17:50:27 +0200

    updated test_net.py

--- test_net.py
+++ test_net.py
@@ -12,6 +12,7 @@ import os, sys
 sys.path.append('/src/')
 
 # import libs
+import numpy as np
 import nibabel as nib
 from base import *
 from build_model import cascade_model
@@ -30,7 +31,7 @@ options['tmp_folder'] = '/tmp/seg'
 options['current_scan'] = 'scan'
 options['modalities'] = ['T1', 'FLAIR']
 options['x_names'] = ['T1_brain.nii.gz', 'FLAIR_brain.nii.gz']
-options['out_name'] = 'out_seg.nii.gz'
+options['out_name'] = 'result.nii.gz'
 exp_folder = os.path.join('/', options['experiment'])
 
 # preprocessing
@@ -64,7 +65,6 @@ except:
 
 os.system('cp ' + options['input_folder'] +'/* ' + options['tmp_folder']+'/')
     
-
 # --------------------------------------------------
 # preprocess the scans
 # FLAIR.nii.gz --> FLAIR_brain.nii.gz
@@ -88,7 +88,6 @@ print "--------------------------------------------------"
 
 options['weight_paths'] = os.path.join('/')
 model = cascade_model(options)
-
     
 # --------------------------------------------------
 # Testing the cascaded model  
@@ -98,10 +97,8 @@ print "--------------------------------------------------"
 print "3. testing scan "
 print "--------------------------------------------------"
 
-
 x_data = {'seg': {'T1': os.path.join(options['tmp_folder'], options['x_names'][0]),
                   'FLAIR': os.path.join(options['tmp_folder'], options['x_names'][1])}}
-
     
 # first network
 options['test_name'] = os.path.join(options['tmp_folder'] , 'seg_prob_0.nii.gz')
@@ -111,9 +108,21 @@ t1 = test_scan(model[0], x_data, options, save_nifti= False)
 options['test_name'] = os.path.join(options['tmp_folder'] , 'seg_prob_1.nii.gz')
 t2 = test_scan(model[1], x_data, options, save_nifti= False, candidate_mask = t1>0.5, test_da = options['test_da'])
 
+# third network (reduce FP)
+out_seg = np.copy(t2)
+t2 = process_segmentation(t2,
+                          min_th = 0.5,
+                          min_size = 30,
+                          dilate =  False)
+
+options['test_name'] = os.path.join(options['tmp_folder'] , 'seg_prob_3.nii.gz')
+t3 = test_scan(model[2], x_data, options, save_nifti= False, candidate_mask = t2>0.5, test_da = options['test_da'])
+options['test_name'] = options['experiment'] + '_prob_1.nii.gz'
+out_seg[t2 > 0.5] = t3[t2>0.5]
+
 # postprocess the output segmentation
 options['test_name'] = os.path.join(options['tmp_folder'] , 'seg_out_CNN.nii.gz')
-out_segmentation = post_process_segmentation(t2, options, save_nifti = False)
+out_segmentation = post_process_segmentation(out_seg, options, save_nifti = False)
 
 # save nifti results
 out_scan = nib.load(x_data['seg']['T1'])
